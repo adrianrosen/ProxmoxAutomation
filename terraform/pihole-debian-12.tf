05be6e8b8e159203229a37753b1560a1ff7aa726
Adrian Rosén <42138011+adrianrosen@users.noreply.github.com>
2023-11-24 10:21:06 +0100

    Added Terraform file for (soon-to-be) PiHole

---
+++ terraform/pihole-debian-12.tf
@@ -0,0 +1,89 @@
+variable "proxmox_node" {
+  type = string
+}
+
+variable "pihole_macaddr" {
+  type = string
+}
+
+variable "pihole_ipconfig0" {
+  type = string
+}
+
+variable "pihole_host" {
+  type = string
+}
+
+variable "pihole_storage" {
+  type = string
+}
+
+resource "proxmox_vm_qemu" "pihole-debian-12" {
+  count = 1
+  name = "pihole-debian-12"
+  desc = "PiHole on Debian 12"
+  vmid = 123
+  target_node = "${var.proxmox_node}"
+  qemu_os = "l26"
+
+  agent = 1
+
+  clone = "debian-12-bookworm"
+  cores = 4
+  sockets = 1
+  memory = 4096
+  cpu = "host"
+  scsihw = "virtio-scsi-pci"
+  
+  network {
+    bridge = "vmbr0"
+    model = "virtio"
+    macaddr = "${var.pihole_macaddr}"
+  }
+
+  os_type = "cloud-init"
+  ipconfig0 = "${var.pihole_ipconfig0}"
+
+  disk {
+    storage = "${var.pihole_storage}"
+    size = "20G"
+    type = "virtio"
+    discard = "ignore"
+    backup = true
+  }
+
+  lifecycle {
+    ignore_changes = [
+      disk.0.cache,
+      disk.0.iops,
+      disk.0.iops_max,
+      disk.0.iops_max_length,
+      disk.0.iops_rd,
+      disk.0.iops_rd_max,
+      disk.0.iops_rd_max_length,
+      disk.0.iops_wr,
+      disk.0.iops_wr_max,
+      disk.0.iops_wr_max_length,
+      disk.0.iothread,
+      disk.0.mbps,
+      disk.0.mbps_rd,
+      disk.0.mbps_rd_max,
+      disk.0.mbps_wr,
+      disk.0.mbps_wr_max,
+      disk.0.replicate
+    ]
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "serveradmin"
+    private_key = file("~/.ssh/id_rsa_packer")
+    host        = "${var.pihole_host}"
+  }
+
+  # Test upload to see if it works
+  provisioner "file" {
+    source      = "../README.md"
+    destination = "/home/serveradmin/README.md"
+  }
+}
